545defaae3de69c4f6c9d8b67a1313d52a6d93af
saro-dev <codersaro@gmail.com>
2024-09-11 12:16:01 +0530

    test

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,16 +1,21 @@
+// src/components/HeroSection.js
 import React from 'react';
-import './HeroSection.css'; // Import the CSS file for styling
-import ModelViewer from './ModelViewer'; // Import ModelViewer
+import ModelViewer from './ModelViewer';
 
 const HeroSection = () => {
   return (
-    <section className="hero-section">
-      <div className="text-container">
-        <h1>Hi, I'm Saravanan</h1>
-        <p>I'm a Web and Flutter Developer with years of experience.</p>
+    <section className="relative w-full h-screen flex items-center justify-center">
+      {/* Transparent overlay with text */}
+      <div className="absolute inset-0 flex flex-col items-start justify-start text-center px-4 md:mt-28">
+        <h1 className=" text-4xl sm:text-6xl font-bold mb-4">Hi, I'm Saravanan</h1>
+        <p className=" text-lg sm:text-2xl">I'm a Web and Flutter Developer <br/> with years of experience.</p>
       </div>
-      <div className="model-container">
-        <ModelViewer modelUrl="./mychar.glb" />
+
+      {/* Model container, centered and full width */}
+      <div className="md:pl-20 relative w-full h-full flex items-center justify-center">
+        <div className="w-full md:h-[95%] h-[60%]">
+          <ModelViewer modelUrl="./mychar.glb" />
+        </div>
       </div>
     </section>
   );
